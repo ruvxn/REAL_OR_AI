ff11fcf2a350a884a5aef1f43f995bc5c87280d4
Ruveen Jayasinghe <ruveenjay@gmail.com>
2025-02-19 23:07:50 +1100

    Update real_or_ai.py

--- real_or_ai.py
+++ real_or_ai.py
@@ -52,34 +52,13 @@ val_df["label"] = val_df["label"].astype(str)
 IMG_SIZE = (224, 224)
 BATCH_SIZE = 32
 
-train_datagen = ImageDataGenerator(
-    rescale=1.0/255.0,
-    horizontal_flip=True,
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    zoom_range=0.2
-)
+train_datagen = ImageDataGenerator(rescale=1.0/255.0,horizontal_flip=True,rotation_range=20,width_shift_range=0.2,height_shift_range=0.2,zoom_range=0.2)
 
 val_datagen = ImageDataGenerator(rescale=1.0/255.0)
 
-train_generator = train_datagen.flow_from_dataframe(
-    dataframe=train_df,
-    x_col="file_name",
-    y_col="label",
-    target_size=IMG_SIZE,
-    batch_size=BATCH_SIZE,
-    class_mode="binary"
-)
+train_generator = train_datagen.flow_from_dataframe(dataframe=train_df,x_col="file_name", y_col="label",target_size=IMG_SIZE,batch_size=BATCH_SIZE,class_mode="binary")
 
-val_generator = val_datagen.flow_from_dataframe(
-    dataframe=val_df,
-    x_col="file_name",
-    y_col="label",
-    target_size=IMG_SIZE,
-    batch_size=BATCH_SIZE,
-    class_mode="binary"
-)
+val_generator = val_datagen.flow_from_dataframe(dataframe=val_df,x_col="file_name",y_col="label",target_size=IMG_SIZE,batch_size=BATCH_SIZE,class_mode="binary")
 
 model = Sequential([
     Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(224, 224, 3)),
@@ -106,23 +85,14 @@ model = Sequential([
     Dense(1, activation='sigmoid')
 ])
 
-model.compile(
-    optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
-    loss='binary_crossentropy',
-    metrics=['accuracy']
-)
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),loss='binary_crossentropy',metrics=['accuracy'])
 
 model.summary()
 
 early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.7, patience=2)
 
-history = model.fit(
-    train_generator,
-    validation_data=val_generator,
-    epochs=15,
-    callbacks=[early_stopping, reduce_lr]
-)
+history = model.fit(train_generator,validation_data=val_generator,epochs=15,callbacks=[early_stopping, reduce_lr])
 
 plt.figure(figsize=(8, 4))
 plt.plot(history.history['accuracy'], label='Train Accuracy')
